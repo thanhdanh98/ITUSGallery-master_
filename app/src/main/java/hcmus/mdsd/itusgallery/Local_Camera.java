aee0593fc76c7a7fadf2e723fb99c7d627923be7
thanhdanh98 <toinguoitayson@gmail.com>
2018-12-17 23:30:34 +0700

    sua bai

--- app/src/main/java/hcmus/mdsd/itusgallery/Local_Camera.java
+++ app/src/main/java/hcmus/mdsd/itusgallery/Local_Camera.java
@@ -1,83 +1,130 @@
 package hcmus.mdsd.itusgallery;
 
-import android.app.Activity;
-import android.content.ContentResolver;
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.vision.CameraSource;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-
+import java.util.List;
 
 public class Local_Camera extends AppCompatActivity {
-    int REQUEST_IMAGE_CAPTURE = 123;
-    ImageView imageView;
-    String photoPath;
+    private ImageView imageview;
+    private static final String IMAGE_DIRECTORY = "/Pictures/TakePhoto/";
+    private int  CAMERA = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local__camera);
-        imageView=findViewById(R.id.imgPreView);
-        //tao intent su dung cho chup anh
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager())!=null){
-            try {
-                File photoFile=CreateImageFile();
-                if(photoFile!=null){
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
+        requestMultiplePermissions();
+        imageview = (ImageView) findViewById(R.id.ImgV);
+        takePhotoFromCamera();
+
 
     }
-
-    public File CreateImageFile() throws IOException {
-        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        String imageFileName="JPEG_"+timeStamp+"_";
-        File storageDir=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File image=File.createTempFile(imageFileName,".jpg",storageDir);
-        photoPath=image.getAbsolutePath();
-        return image;
+    private void takePhotoFromCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_IMAGE_CAPTURE){
-            setPic();
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == CAMERA) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(thumbnail);
+            saveImage(thumbnail);
+            Toast.makeText(Local_Camera.this, "Image Saved!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void setPic(){
-        int targetW=imageView.getWidth();
-        int targetH=imageView.getHeight();
-        BitmapFactory.Options options=new BitmapFactory.Options();
-        BitmapFactory.decodeFile(photoPath, options);
-        int photoW=options.outWidth;
-        int photoH=options.outHeight;
-        int scaleFactor=Math.min(photoH/targetH,photoW/targetW);
-        options.inSampleSize=scaleFactor;
-        Bitmap bitmap=BitmapFactory.decodeFile(photoPath,options);
-        imageView.setImageBitmap(bitmap);
-    }
-}
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+        }
 
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+
+    private void  requestMultiplePermissions(){
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+                            //openSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+}
